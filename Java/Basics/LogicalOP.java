9144130ae9b67a0f56e173a2a1a451fb9ce88591
JirenXmoksh <mokshsingh4@gmail.com>
2024-03-17 17:38:17 +0530

    Removed the pushed Java folder

--- Java/Basics/LogicalOP.java
+++
@@ -1,9 +0,0 @@
-public class LogicalOP {
-    public static void main(String[] args) {
-        int num1 = 10, num2 = 20, num3 = 30;
-         System.out.println(num1>num2 &&  num1>num3); // False
-         System.out.println(num1<num2 &&  num1<num3);  // True
-         System.out.println(num1<num2 || num1>num2);  // True
-       // etc...
-    }
-}
